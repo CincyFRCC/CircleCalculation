10c75b704c3b263d82b04e02dbe4b7e882687fc5
cincy <cincy123@gmail.com>
2024-02-05 19:00:53 -0700

    changes

--- src/CircleCalculation.java
+++ src/CircleCalculation.java
@@ -4,16 +4,44 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class CircleCalculation {
     public static void main(String[] args) {
-        double radius,diameter, circumference,area,pi;
-        pi=Math.PI;
+        double radius,area,length,volume;
+
         Scanner rd= new Scanner(System.in);
-        System.out.print("Enter the radius:");
+        System.out.print("Enter the radius of a cylinder:");
         radius=rd.nextDouble();
+        area=computeArea(radius);
+        System.out.printf("Area is: %.2f\n", area);
+        Scanner lt= new Scanner(System.in);
+        System.out.print("Enter the length of a cylinder:");
+        length=lt.nextDouble();
+        volume=computeVolume(area,length);
+        System.out.printf("volume is: %.2f\n", volume);
+        //double radius,diameter, circumference,area,pi;
+        //pi=Math.PI;
+        //Scanner rd= new Scanner(System.in);
+        //System.out.print("Enter the radius:");
+        //radius=rd.nextDouble();
+        //diameter=circleDiameter(radius);
+        //System.out.printf("Diameter is: %.2f\n", diameter);
+        //area=pi*radius*radius;
+        //circumference=2*pi*radius;
+        //System.out.printf("Diameter is: %.2f\n", diameter);
+        //System.out.printf("circumference is: %.2f\n", circumference);
+        //System.out.printf("circumference is: %.2f\n", area);
+    }
+    public  static double computeArea(double radius)
+    {
+        double pi=Math.PI;
+        return radius*radius*pi;
+    }
+    public  static double computeVolume(double area,double length)
+    {
+        return area * length;
+    }
+    public  static double circleDiameter(double radius)
+    {
+        double diameter;
         diameter=2*radius;
-        area=pi*radius*radius;
-        circumference=2*pi*radius;
-        System.out.printf("Diameter is: %.2f\n", diameter);
-        System.out.printf("circumference is: %.2f\n", circumference);
-        System.out.printf("circumference is: %.2f\n", area);
+        return diameter;
     }
 }
