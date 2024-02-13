7de48b53375bf15d050f62f4020cfc8b34ea264a
cincy sebastian <cincy123@gmail.com>
2024-02-13 12:04:47 -0700

    updation

--- src/CircleCalculation.java
+++ src/CircleCalculation.java
@@ -4,44 +4,43 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class CircleCalculation {
     public static void main(String[] args) {
-        double radius,area,length,volume;
 
-        Scanner rd= new Scanner(System.in);
-        System.out.print("Enter the radius of a cylinder:");
-        radius=rd.nextDouble();
-        area=computeArea(radius);
+        double radius, diameter, circumference, area, pi;
+        pi = Math.PI;
+        //Read the radius of Circle
+        Scanner rd = new Scanner(System.in);
+        System.out.print("Enter the radius:");
+        radius = rd.nextDouble();
+        //Calculating Diameter
+        diameter = circleDiameter(radius);
+        //Printing the Diameter of the Circle
+        System.out.printf("Diameter is: %.2f\n", diameter);
+        //Calculating Area
+        area = circleArea(radius);
+        //Printing the Area of the Circle
         System.out.printf("Area is: %.2f\n", area);
-        Scanner lt= new Scanner(System.in);
-        System.out.print("Enter the length of a cylinder:");
-        length=lt.nextDouble();
-        volume=computeVolume(area,length);
-        System.out.printf("volume is: %.2f\n", volume);
-        //double radius,diameter, circumference,area,pi;
-        //pi=Math.PI;
-        //Scanner rd= new Scanner(System.in);
-        //System.out.print("Enter the radius:");
-        //radius=rd.nextDouble();
-        //diameter=circleDiameter(radius);
-        //System.out.printf("Diameter is: %.2f\n", diameter);
-        //area=pi*radius*radius;
-        //circumference=2*pi*radius;
-        //System.out.printf("Diameter is: %.2f\n", diameter);
-        //System.out.printf("circumference is: %.2f\n", circumference);
-        //System.out.printf("circumference is: %.2f\n", area);
+        //Calculating circumference
+        circumference = circleCircumference(radius);
+        //Printing the circumference of the Circle
+        System.out.printf("Circumference is: %.2f\n", circumference);
+
     }
-    public  static double computeArea(double radius)
-    {
-        double pi=Math.PI;
-        return radius*radius*pi;
+
+    public static double circleArea(double radius) {
+        double pi = Math.PI;
+        return radius * radius * pi;
     }
-    public  static double computeVolume(double area,double length)
-    {
-        return area * length;
-    }
-    public  static double circleDiameter(double radius)
-    {
+
+    public static double circleDiameter(double radius) {
         double diameter;
-        diameter=2*radius;
+        diameter = 2 * radius;
         return diameter;
+    }
+
+    public static double circleCircumference(double radius) {
+        double pi = Math.PI;
+        double circumference;
+        circumference = 2 * pi * radius;
+        return circumference;
     }
 }
